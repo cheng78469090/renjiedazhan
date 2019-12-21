5399f7b8c6d9692c93e1f150613d378b3e375e72
宋金城 <1156920605@qq.com>
2019-12-21 14:06:59 +0800

    明天冬至

---
+++ javaDemos/src/com/atguigu/test1218/Student.java
@@ -0,0 +1,14 @@
+package com.atguigu.test1218;
+
+/**
+ * @Auther: 宋金城
+ * @Date: 2019/12/20 21:08
+ * @Description:
+ */
+public class Student {
+    public static void main(String[] args) {
+
+        double i=Math.PI;
+    }
+    private int id;
+}
